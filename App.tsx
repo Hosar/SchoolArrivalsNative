f47204a320b85103efe69c90fd905ac8e2212b93
Hosar <hosarsiph.valle@gmail.com>
2024-02-23 15:31:11 -0600

    First scratch

---
+++ App.tsx
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import Crashes from 'appcenter-crashes';
+import Analytics from 'appcenter-analytics';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const crashApp = () => {
+    Crashes.generateTestCrash();
+    // throw new Error('Simple Error Test');
+  };
+
+  const sendEvent = () => {
+    console.log('Sending Event');
+    Analytics.trackEvent('New Event');
+  };
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Button title="Crash App" onPress={crashApp} />
+      <Button title="Send Event" onPress={sendEvent} />
+    </SafeAreaView>
+  );
+}
+
+export default App;
